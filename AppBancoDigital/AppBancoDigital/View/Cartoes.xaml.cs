64e9f0079151163a7eea78ef565ae0a3a10d5679
marianepagagnotti <marianepagagnotti.sl@gmail.com>
2023-09-11 14:33:23 -0300

    terminando cartao design

--- AppBancoDigital/AppBancoDigital/View/Cartoes.xaml.cs
+++ AppBancoDigital/AppBancoDigital/View/Cartoes.xaml.cs
@@ -18,6 +18,7 @@ namespace AppBancoDigital.View
             NavigationPage.SetHasNavigationBar(this, false);
             btn_voltar.Source = ImageSource.FromResource("AppBancoDigital.NovaPasta1.seta.png");
             btn_interrogacao.Source = ImageSource.FromResource("AppBancoDigital.NovaPasta1.interrogacao.png");
+            simbolo_cartao_digital.Source = ImageSource.FromResource("AppBancoDigital.NovaPasta1.simbolo.png");
             simbolo_cartao.Source = ImageSource.FromResource("AppBancoDigital.NovaPasta1.simbolo.png");
         }
 
@@ -32,6 +33,11 @@ namespace AppBancoDigital.View
         }
 
         private void btn_dados_cartaofisico_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_dados_cartaodigital_Clicked(object sender, EventArgs e)
         {
 
         }
