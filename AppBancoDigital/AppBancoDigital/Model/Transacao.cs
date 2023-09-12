d3d56e496f8a05f4c237bd34ddc1533488a8da5d
marianepagagnotti <marianepagagnotti.sl@gmail.com>
2023-09-12 14:53:30 -0300

    começando pix

---
+++ AppBancoDigital/AppBancoDigital/Model/Transacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBancoDigital.Model
+{
+    public class Transacao
+    {
+        public int Id { get; set; }
+        public string Valor { get; set; }
+        public DateTime Data { get; set; }
+        public int conta_remetente { get; set; }
+        public int conta_destinatario { get; set; }
+    }
+}
